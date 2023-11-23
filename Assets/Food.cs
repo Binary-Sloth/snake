e2a0786bf6b7e74bd5a4e6f297ad12dd3ee1a93a
Katy Wei <katywei.delft@gmail.com>
2023-11-23 17:35:18 -0600

    collide snake with food

--- Assets/Food.cs
+++ Assets/Food.cs
@@ -20,4 +20,12 @@ public class Food : MonoBehaviour
         this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
 
     }
+
+    // action when collision occurs
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player") {
+            RandomizePosition();
+        }
+    }
 }
