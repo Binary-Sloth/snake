d7291bf06d4a370b2dba2144c4074019539d0a8c
Katy Wei <katywei.delft@gmail.com>
2023-11-23 17:08:31 -0600

    add snake movement and food initialisation

--- Assets/Snake.cs
+++ Assets/Snake.cs
@@ -1,18 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 _direction = Vector2.right;
+
+    // Update() is called every frame
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W)) {
+            _direction = Vector2.up;
+        } else if (Input.GetKeyDown(KeyCode.S)) {
+            _direction = Vector2.down;
+        } else if (Input.GetKeyDown(KeyCode.A)) {
+            _direction = Vector2.left;
+        } else if (Input.GetKeyDown(KeyCode.D)) {
+            _direction = Vector2.right;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // FixedUpdate() is called at fixed time intervals
+    // frame-rate independent
+    // important for physics (don't use Update())
+    private void FixedUpdate()
     {
-        
-    }
+        // 3D vector for position, even in 2D game
+        this.transform.position = new Vector3(
+            Mathf.Round(this.transform.position.x) + _direction.x,
+            Mathf.Round(this.transform.position.y) + _direction.y,
+            0.0f
+        );
+    }   
 }
+ 
