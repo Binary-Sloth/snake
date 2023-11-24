423fcd766ff6d65cef9f98e340549eb7e91e17b3
Katy Wei <katywei.delft@gmail.com>
2023-11-23 18:25:03 -0600

    grow snake

--- Assets/Snake.cs
+++ Assets/Snake.cs
@@ -1,12 +1,24 @@
 
+using System.Collections.Generic; // necessary to define list
 using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
     private Vector2 _direction = Vector2.right;
+    // a Transform is like an object
+    private List<Transform> _segments;
+    public Transform segmentPrefab;
 
-    // Update() is called every frame
+
+    private void Start()
+    // initialise snake with head
+    {
+        _segments = new List<Transform>();
+        _segments.Add(this.transform);
+    }
+
     private void Update()
+    // Update() is called every frame
     {
         if (Input.GetKeyDown(KeyCode.W)) {
             _direction = Vector2.up;
@@ -19,11 +31,17 @@ public class Snake : MonoBehaviour
         }
     }
 
+    private void FixedUpdate()
     // FixedUpdate() is called at fixed time intervals
     // frame-rate independent
     // important for physics (don't use Update())
-    private void FixedUpdate()
     {
+        // move each segment from tail to head
+        for (int i = _segments.Count - 1; i > 0; i--){
+            _segments[i].position = _segments[i - 1].position;
+        }
+
+        
         // 3D vector for position, even in 2D game
         this.transform.position = new Vector3(
             Mathf.Round(this.transform.position.x) + _direction.x,
@@ -31,5 +49,23 @@ public class Snake : MonoBehaviour
             0.0f
         );
     }   
+
+    private void Grow()
+    {
+        // clone prefab asset
+        Transform segment = Instantiate(this.segmentPrefab);
+        // set position of new segment to the current snake tail
+        segment.position = _segments[_segments.Count - 1].position;
+        _segments.Add(segment);
+    }
+
+    // action when collision occurs
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Food") {
+            Grow();
+        }
+    }
+
 }
  
