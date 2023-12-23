16b452570f6fecaf2df44c3d4d9d5c42a19ceef1
Katy Wei <katywei.delft@gmail.com>
2023-12-23 14:18:34 -0600

    make powerups disappear after 30 seconds

--- Assets/Scripts/PowerUps/PowerUp.cs
+++ Assets/Scripts/PowerUps/PowerUp.cs
@@ -2,13 +2,17 @@ using UnityEngine;
 
 public class PowerUp : Food
 {
-    // common features for all powerups
+    // powerup duration on screen
+    private float duration = 30f;
 
     protected override void Start()
     {
         base.Start();
-        // deduct points if powerUp is used
-        points = -100;
+        // set points for each powerup
+        points = 0;
+
+        // Destroy after duration seconds
+        Destroy(this.gameObject, duration);
     }
 
     protected override void OnTriggerEnter2D(Collider2D other)
