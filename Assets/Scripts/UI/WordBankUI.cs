70e4af517c47c23aefc37c93677395becaaca61a
Katy Wei <katywei.delft@gmail.com>
2023-12-30 12:18:29 -0600

    move word bank upon overflow

--- Assets/Scripts/UI/WordBankUI.cs
+++ Assets/Scripts/UI/WordBankUI.cs
@@ -1,11 +1,30 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using Unity.VisualScripting;
 
 public class WordBankUI : TextUI
 {
+    Vector2 startPosition;
+    TextMeshPro textbox;
+
+    void Start()
+    {
+        startPosition = new Vector2(
+            Mathf.RoundToInt(transform.position.x),
+            Mathf.RoundToInt(transform.position.y)
+        );
+        
+        TextMeshPro textbox = this.GetComponentInChildren<TextMeshPro>();
+    }
+    
     void Update()
     {
-        this.GetComponentInChildren<TextMeshPro>().text = wordManager.wordBankString;
+        textbox = this.GetComponentInChildren<TextMeshPro>();
+        textbox.text = wordManager.wordBankString;
+
+        if (textbox.isTextOverflowing) {
+            transform.position = startPosition + new Vector2(2, 0);
+        }
     }
 }
